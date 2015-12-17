8b4615daacdac3ae72d81da54d1b1f77e7da3133
Kaizhe Huang <hkz85825915@gmail.com>
2015-12-17 15:12:03 +0800

    Replace SVG logo to PNG for unsupported browsers

--- assets/static/old-browsers.js
+++ assets/static/old-browsers.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-	//Internet Explorer
-	if ( /MSIE/.test(navigator.userAgent) ) {
+	//SVG in img
+	if ( /MSIE [2-8]|Android 2.[0-3]/.test(navigator.userAgent) ) {
 		$(".logo, .small-logo").attr("src", $(".logo, .small-logo").attr("src").replace("logo.svg", "favicon-192.png"));
 	}
 
