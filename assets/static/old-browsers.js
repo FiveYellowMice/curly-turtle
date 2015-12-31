10a31eb98c810ff843c7da47fc70e5e02e3dd00d
Kaizhe Huang <hkz85825915@gmail.com>
2015-12-31 11:42:58 +0800

    Fallback SVG onerror

--- assets/static/old-browsers.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-	//SVG in img
-	if ( /MSIE [2-8]|Android 2.[0-3]/.test(navigator.userAgent) ) {
-		$(".logo, .small-logo").attr("src", $(".logo, .small-logo").attr("src").replace("logo.svg", "favicon-192.png"));
-	}
-
-});
